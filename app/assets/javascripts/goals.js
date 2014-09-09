6f34f9b8518e0d8c5e4039e83e681c1840540814
Jon Lyons <jrl08d@my.fsu.edu>
2014-09-09 06:28:22 -0400

    added fullcalender jquery

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+         header: 
+         {
+         	left:   'title',
+         	center: 'basicWeek',
+         	right:  'today prev,next'
+         }
+    })
+
+});
