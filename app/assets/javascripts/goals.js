63a7084d11b595015eda6d49c7fecc61820442d6
Jon Lyons <jrl08d@my.fsu.edu>
2014-10-07 22:03:56 -0400

    made goals calendar transparent

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -1,15 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready( function() {	
 
 
 
+
+$(document).ready(function() {
 	$( ":checkbox" ).unbind('click');
 	$( ":checkbox" ).click( function() {
 		var task = $(this).parent().parent();
 		task.toggleClass('checked');
-		});
-		return false;
+	});
+	return false;
 });  
 
