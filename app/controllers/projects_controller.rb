e5720843b4d93d7fb6e0611cca20e825e8b91150
Corvo <jodamaru@gmail.com>
2014-10-12 23:14:39 -0400

    Added post_img to show and project show - itminimizes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -72,6 +72,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:description, :title, :default, :user_id, :avatar)
+      params.require(:project).permit(:description, :title, :default, :user_id, :post_img)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,22 @@
 class Post < ActiveRecord::Base
   belongs_to :project
-  has_many :discussions, dependent: :destroy		# corvo - Relates Discussions to Posts and destroys dependencies upon deletion
-
-  # corvo - Added paperclip to Post ( for attaching files )
-  has_attached_file :post_img 
-  validates_attachment :post_img, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   
+  # corvo - Validates presence of both text and post image
+  validates :post_text, presence: true
+  validates :post_img, presence: true
 
+  # corvo - Relates Discussions to Posts and destroys dependencies upon deletion
+  has_many :discussions, dependent: :destroy		
 
+  # corvo - Added paperclip to Post ( for attaching files ), beneath, conditionals
+  has_attached_file :post_img, :styles => { :small => "150x150>",
+  											:medium => "400x400>",
+  											:large => "800x800>"} #,
+  					# :url => "/assets/posts/:id/:style/:basename.:extension",
+  					# :path => ":rails_root/:public/assets/posts/:id/:style/:basename.:extension"
+
+  # corvo - validations in size and kind
+  validates_attachment :post_img, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_with AttachmentSizeValidator, :attributes => :post_img, :less_than => 1.megabytes
 
 end
